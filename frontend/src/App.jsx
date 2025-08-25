cd1e7b7272e384999a5d4bcca1118eaff4041313
alejandroberod <luisbetancourth43@gmail.com>
2025-08-25 17:47:04 -0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
+
+function App() {
+
+  return (
+    <>
+      <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
