d2b554b1587425c6d6130d9446bab786a0677183
alejandroberod <luisbetancourth43@gmail.com>
2025-08-26 12:23:36 -0500

    Advance in the navBar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { HiMenu, HiX } from 'react-icons/hi';
+import { motion } from 'motion/react'
 
+import { images } from '../../constants';
 import './Navbar.scss'
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="" />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='app__navbar-menu'>
+         <HiMenu onClick={() => setToggle(true)}/>
+      </div>
+    </nav>
   )
 }
