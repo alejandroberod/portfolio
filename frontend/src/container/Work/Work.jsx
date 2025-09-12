2a548755c6710281427a0838b3d8b8f49e85af68
alejandroberod <luisbetancourth43@gmail.com>
2025-09-11 21:10:40 -0500

    Changes in work section

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "motion/react";
 
-import './Work.scss'
+import { AppWrap } from "../../wrapper";
+
+import "./Work.scss";
+import { div } from "motion/react-client";
 
 export default function Work() {
+  const [activeFilter, setActiveFilter] = useState('All');
+
+  function handleWorkFilter(item) {}
+
   return (
-    <div>Work</div>
-  )
+    <>
+      <h2 className="head-text">
+        My creative <span>Portfolio</span> section
+        <div className="app__work-filter">
+          {["JavaScript", "React.Js", "Node.Js", "AI", "All"].map(
+            (item, index) => (
+              <div
+                key={index}
+                onClick={() => handleWorkFilter(item)}
+                className={`app__work-filter-item app__flex p-text ${
+                  activeFilter === item ? "item-active" : ""
+                }`}
+              >
+                {item}
+              </div>
+            )
+          )}
+        </div>
+      </h2>
+    </>
+  );
 }
